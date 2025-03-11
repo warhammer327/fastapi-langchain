037adc60651dfa43def9bbe6e842fc8de54d7f95
Syed Rizwan <rizwansm@outlook.com>
2025-03-12 00:09:54 +0600

    test commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+import os
+
 import requests
 from fastapi import FastAPI, HTTPException
-import os
 
 OLLAMA_HOST = os.getenv("OLLAMA_HOST")
 app = FastAPI(title="Tragic End")
@@ -8,13 +9,13 @@ app = FastAPI(title="Tragic End")
 
 @app.get("/")
 def read_root():
-    return {"message": "check check check"}
+    return {"message": "check test check check"}
+
 
 @app.get("/models-list")
 def list_models():
     try:
-        response = requests.get(
-            f"{OLLAMA_HOST}/api/tags")
+        response = requests.get(f"{OLLAMA_HOST}/api/tags")
         return response.json()
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error listing models: {str(e)}")
